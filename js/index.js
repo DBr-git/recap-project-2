6e0ea4b882397b2d41b8704519e9970c6270b64e
Dennis <d-l-b@gmx.de>
2024-11-08 10:39:52 +0100

    added interactivity to bookmark and answer button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// phew… not a lot going on here. Please add some code!
+console.clear();
+
+// Adds interactive Bookmark Icon
+const bookmark = document.querySelector('[data-js="bookmark"]');
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("bookmark--active");
+});
+
+// Adds interactive Answer Button
+const showAnswerButton = document.querySelector('[data-js="showAnswerButton"]');
+const answer = document.querySelector('[data-js="answer"]');
+
+showAnswerButton.addEventListener("click", () => {
+  answer.classList.toggle("card__answer--active");
+});
